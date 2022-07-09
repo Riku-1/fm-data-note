08f966527e18d320d75ba236d44c837b5af95bae
Riku-1 <osaka.riku@gmail.com>
2022-07-09 13:00:07 +0900

    feature(#4):added team member's num display

--- src/web/page/MyClubNotePage/Summary.tsx
+++ src/web/page/MyClubNotePage/Summary.tsx
@@ -9,12 +9,15 @@ export type SummaryProps = {
 export const Summary = (props: SummaryProps) => {
     return (
         <div>
+            <div>Team: { props.currentPlayers.filter(player => {
+                return player.isMember
+            }).length }</div>
             <div>ClubGrown(T): { props.currentPlayers.filter(player => {
                 return !player.isPlanToRelease && player.homeGrownStatus === HomeGrownStatus.Club
             }).length }</div>
 
             <div>ClubGrown(M): { props.currentPlayers.filter(player => {
-                return player.isPlanToRelease && player.isMember && player.homeGrownStatus === HomeGrownStatus.Club
+                return !player.isPlanToRelease && player.isMember && player.homeGrownStatus === HomeGrownStatus.Club
             }).length }</div>
 
             <div>NationGrown(T): { props.currentPlayers.filter(player => {
@@ -22,7 +25,7 @@ export const Summary = (props: SummaryProps) => {
             }).length }</div>
 
             <div>NationGrown(M): { props.currentPlayers.filter(player => {
-                return player.isPlanToRelease && player.isMember && player.homeGrownStatus === HomeGrownStatus.Nation
+                return !player.isPlanToRelease && player.isMember && player.homeGrownStatus === HomeGrownStatus.Nation
             }).length }</div>
 
             <div>U20: { props.currentPlayers.filter(player => {
